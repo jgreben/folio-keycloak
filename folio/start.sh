c651f097193efe90a400ff8e59cb3ea8746e298c
msinichenko <159009128+msinichenko@users.noreply.github.com>
2024-03-07 09:26:18 +0200

    Update start.sh

--- folio/start.sh
+++ folio/start.sh
@@ -21,5 +21,6 @@ else
   /opt/keycloak/bin/kc.sh start \
    --optimized --http-enabled=false --https-key-store-type=BCFKS \
    --https-key-store-file="${KC_HTTPS_KEY_STORE:-/opt/keycloak/conf/test.server.keystore}" --https-key-store-password="${KC_HTTPS_KEY_STORE_PASSWORD:-SecretPassword}" \
+   --spi-password-hashing-pbkdf2-sha256-max-padding-length=14 \
    --log-level=INFO,org.keycloak.common.crypto:TRACE,org.keycloak.crypto:TRACE
 fi
